9f60a41970e0203b3266cce9cff56a85fc174c5d
Veming <34410996+Veming@users.noreply.github.com>
2018-06-14 19:33:55 +0800

    Update Main.java

--- src/main/java/com/hrbust/su/topology/Main.java
+++ src/main/java/com/hrbust/su/topology/Main.java
@@ -5,10 +5,14 @@ import com.hrbust.su.spouts.S1;
 import com.hrbust.su.spouts.S2;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.topology.TopologyBuilder;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
 
 		// TODO Auto-generated method stub
 
@@ -26,9 +30,16 @@ public class Main {
 
 		builder.setBolt("B3", new B3()).shuffleGrouping("B21").shuffleGrouping("B23").shuffleGrouping("B22");
 
-      		Config conf = new Config();
-		conf.setNumWorkers(3);
-		StormSubmitter.submitTopology("topology", conf, builder.createTopology());
+		Config conf = new Config();
+		if (args.length>0){
+			conf.setNumWorkers(3);
+			StormSubmitter.submitTopology("topology", conf, builder.createTopology());
+		}
+        else{
+      		  	LocalCluster cluster = new LocalCluster();
+       	  	  	cluster.submitTopology("topology", conf, builder.createTopology());
+		}
+
 
 	}
 
